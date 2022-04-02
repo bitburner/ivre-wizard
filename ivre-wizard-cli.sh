d795e350373ce89e3405b587b6504a4bac44cadc
BitBurner <bitburner@gmail.com>
2022-04-02 03:09:23 -0700

    Update ivre-wizard-cli.sh

--- ivre-wizard-cli.sh
+++ ivre-wizard-cli.sh
@@ -114,7 +114,7 @@ case $yn in
 	[Yy]*) echo -e "⏳ ${GREEN}Scan Started as $USER ${NOCOLOR}" &&
 	ivre runscans ${scan} ${ask} --limit $setlimit --output=XMLFork --processes $setprocesses &&
 	echo -e "⏳ ${GREEN}Import Started as $USER ${NOCOLOR}" &&
-	ivre scan2db -c ${category//[[:blank;]]/} -s ${scansource//[[:blank;]]/} -r scans/${cat}/up/*;
+	ivre scan2db -c ${category//[[:blank;]]/} -s ${scansource//[[:blank;]]/} -r scans/${category}/up/*;
 	echo -e "⌛ ${GREEN}Creating View from imported scans as $USER ${ORANGE}Be patient, this may take a while...${NOCOLOC}" &&
 	sudo ivre db2view nmap &&
 	echo -e "🛠 ${RED}Removing Scans after import. ${ORANGE}Almost done...${NOCOLOR}" &&
